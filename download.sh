19d5cfb4ad3f202af9c67cbede76bb4aa5091b70
Gaurav <gaurav.sablok@uni-potsdam.de>
2024-06-14 11:58:01 +0200

    better organization 2024-6-14

--- download.sh
+++
@@ -1,2 +0,0 @@
-curl https://www.arabidopsis.org/download/file?path=GO_and_PO_Annotations/Plant_Ontology_Annotations/po_anatomy_gene_arabidopsis_tair.assoc.gz -o po_anatomy_gene_arabidopsis_tair.assoc.gz
-gunzip po_anatomy_gene_arabidopsis_tair.assoc.gz
